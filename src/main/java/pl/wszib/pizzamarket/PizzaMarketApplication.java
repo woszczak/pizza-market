bec02b5206bf371b479eba64b6d6a670ba1f3160
Justyna Woszczak <justynawoszczak1@gmail.com>
2023-05-20 14:21:29 +0200

    Strona Główna

---
+++ src/main/java/pl/wszib/pizzamarket/PizzaMarketApplication.java
@@ -0,0 +1,12 @@
+package pl.wszib.pizzamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaMarketApplication.class, args);
+	}
+}
